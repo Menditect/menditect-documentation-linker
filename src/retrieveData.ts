8efd014fdf62939856fe4eca7f66da1221bf6aec
Vince van Noort <v.vannoort@egalit.nl>
2022-05-16 17:22:58 +0200

    remove unnecessary import

--- src/retrieveData.ts
+++ src/retrieveData.ts
@@ -1,6 +1,4 @@
-import { MenditectDocumentationLinkerContainerProps } from "../typings/MenditectDocumentationLinkerProps";
-
-export function retrieveData(link: string) {
+export function retrieveData(link: string): Promise<any> {
     const baseLinkCleaned = link.replace(/\/$/, "");
     const docusaurusSearchJsonUrl = `${baseLinkCleaned}/search-index-docs-default-current.json`;
     return fetch(docusaurusSearchJsonUrl)
